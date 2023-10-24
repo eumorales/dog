1398ef1980fe56cac7510560726ffff2bf057cdc
Gilberto <124392451+eumorales@users.noreply.github.com>
2023-10-24 16:43:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const imagem = document.getElementById('imagem');
+
+async function randomCachorro() {
+    try {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        if (response.ok) {
+            const data = await response.json();
+            const imageUrl = data.message;
+            imagem.src = imageUrl;
+        } else {
+            throw new Error('Algo deu errado na hora de carregar a imagem. :(');
+        }
+    } catch (error) {
+        console.error(error);
+        randomCachorro();
+
+    }
+}
+
+randomCachorro();
+setInterval(randomCachorro, 5000);
